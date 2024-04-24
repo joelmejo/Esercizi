77d861b2c48bb18db43acf4953d9a7f38e83174f
joelmejo <167082021+joelmejo@users.noreply.github.com>
2024-04-24 13:56:38 +0200

    Lezione4 bubble sort

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -62,3 +62,62 @@
 # import module_name as mn
 # from module_name import *
 # 8-17. Styling Functions: Choose any three programs you wrote for this chapter, and make sure they follow the styling guidelines described in this section.
+
+
+
+###########
+# Implement Bubble Sort
+
+import time
+
+numbers: list = []
+
+for x in range(1, 10000 + 1):
+    numbers.append(x)
+
+def swap(x: int, y: int):
+    return y, x
+
+def bubble_sort(a: list):
+    for i in range(len(a)):
+        for j in range(len(a) - 1):
+            if(a[j] > a[j + 1]):
+                a[j], a[j + 1] = swap(a[j], a[j +1])
+    return None
+start1: int = time.time()
+bubble_sort(numbers)
+print(numbers)
+print(f"{time.time() - start1} -- PRIMA VERSIONE")
+
+
+# Improved one
+
+def improved_bubble_sort(a: list):
+    for i in range(len(a)):
+        for j in range(len(a) - i - 1):
+            if(a[j] > a[j + 1]):
+                a[j], a[j + 1] = swap(a[j], a[j +1])
+    return None
+
+start2: int = time.time()
+improved_bubble_sort(numbers)
+print(numbers)
+print(f"{time.time() - start2} -- SECONDA VERSIONE")
+
+# Flag bubble sort
+
+def flag_bubble_sort(a: list):
+    for i in range(len(a)):
+        swap_flag = False
+        for j in range(len(a) - i - 1):
+            if(a[j] > a[j + 1]):
+                swap_flag = True
+                a[j], a[j + 1] = swap(a[j], a[j +1])
+        if swap_flag is False:
+            return None
+    return None
+
+start3: int = time.time()
+flag_bubble_sort(numbers)
+print(numbers)
+print(f"{time.time() - start3} -- TERZA VERSIONE")
