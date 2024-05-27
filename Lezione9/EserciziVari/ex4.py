a9c3a8e2929a9572ede6037aa1c904acf48244f4
joelmejo <167082021+joelmejo@users.noreply.github.com>
2024-05-27 22:07:40 +0200

    Lezione9 ex7 to do

--- Lezione9/EserciziVari/ex4.py
+++ Lezione9/EserciziVari/ex4.py
@@ -14,3 +14,28 @@
 #             create_account(account_id): crea un nuovo account con l'ID specificato e un saldo pari a 0.
 #             deposit(account_id, amount): deposita l'importo specificato sul conto con l'ID fornito.
 #             get_balance(account_id): restituisce il saldo del conto con l'ID specificato.
+
+class Account:
+    def __init__(self, account_id: str, balance=0) -> None:
+        self.account_id = account_id
+        self.balance = balance
+        
+    def deposit(self, amount: float) -> None:
+        self.balance += amount
+    
+    def get_balance(self) -> float:
+        return self.balance
+
+
+class Bank:
+    def __init__(self, accounts: dict[str, Account]) -> None:
+        self.accounts = accounts
+    
+    def create_accounts(self, account_id: str) -> None:
+        self.accounts[account_id] = Account(account_id)
+    
+    def deposit(self, account_id: str, amount: float) -> None:
+        self.accounts[account_id].deposit(amount)
+    
+    def get_balance(self, account_id) -> float:
+        return self.accounts[account_id].get_balance()
--- Lezione9/EserciziVari/ex5.py
+++ Lezione9/EserciziVari/ex5.py
@@ -29,3 +29,53 @@
 #             borrow_book(member_id: str, book_id: str): Permette al membro di prendere in prestito il libro.
 #             return_book(member_id: str, book_id: str): Permette al membro di restituire il libro.
 #             get_borrowed_books(member_id): list[Book] - restituisce la lista dei libri presi in prestito dal membro.
+
+class Book:
+    def __init__(self, book_id: str, title: str, author: str, is_borrowed: bool) -> None:
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.is_borrowed = is_borrowed
+    
+    def borrow(self):
+        if self.is_borrowed == False:
+            self.is_borrowed = True
+    
+    def return_book(self) -> None:
+        self.is_borrowed = False
+
+class Member:
+    def __init__(self, member_id: str, name: str, borrowed_books: list[Book]) -> None:
+        self.member_id = member_id
+        self.name = name
+        self.borrowed_books = borrowed_books
+    
+    def borrow_book(self, book: Book):
+        self.borrowed_books.append(book)
+        
+    def return_book(self, book: Book):
+        self.borrowed_books.remove(book)
+
+
+class Library:
+    def __init__(self, books: dict[str, Book], members: dict[str, Member]) -> None:
+        self.books = books
+        self.members = members
+    
+    def add_book(self, book_id: str, title: str, author: str) -> None:
+        self.books[book_id] = Book(book_id, title, author)
+
+    def register_member(self, member_id: str, name: str) -> None:
+        self.members[member_id] = Member(member_id, name)
+    
+    def borrow_book(self, member_id: str, book_id: str) -> None:
+        self.books[book_id].borrow()
+        self.members[member_id].borrow_book()
+
+    def return_book(self, member_id: str, book_id: str) -> None:
+        self.books[book_id].return_book()
+        self.members[member_id].return_book()
+    
+    def get_borrowe_books(self, member_id) -> list[Book]:
+        return self.members[member_id].borrowed_books
+
