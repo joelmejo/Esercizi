8add4397e3fbc103570b0378007990c723e8faeb
joelmejo <167082021+joelmejo@users.noreply.github.com>
2024-05-31 10:30:16 +0200

    minor

--- Lezione8/classi_astratte.py
+++ Lezione8/classi_astratte.py
@@ -2,7 +2,7 @@
 
 # Create an abstract class Shape with an abstract method area and another abstract method perimeter.
 # Then, create two subclasses Circle and Rectangle that implement the area and perimeter methods.
-
+from abc import ABC, abstractmethod
 
 
 
