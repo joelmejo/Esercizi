03dfe9bbea2683f3a14dc18049bfa025fbabf1fb
joelmejo <167082021+joelmejo@users.noreply.github.com>
2024-05-31 13:32:29 +0200

    exercise3 to do

--- Lezione8/classi_astratte.py
+++ Lezione8/classi_astratte.py
@@ -3,14 +3,58 @@
 # Create an abstract class Shape with an abstract method area and another abstract method perimeter.
 # Then, create two subclasses Circle and Rectangle that implement the area and perimeter methods.
 from abc import ABC, abstractmethod
+from math import pi
 
+class Shape(ABC):
 
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape): 
+    def __init__(self, radius: int) -> None:
+        self.radius = radius
+
+    # ovveriding abstract method
+    def perimeter(self) -> float:
+        return 2 * pi * self.radius
+    
+    # overriding abstract method
+    def area(self) -> float:
+        return pi * self.radius**2
+
+class Rectangle(Shape):
+    def __init__(self, short: float, long: float) -> None:
+        self.short = short
+        self.long = long
+
+    # overriding abstract method
+    def perimeter(self) -> float:
+        return (self.short * 2) + (self.long * 2)
+    
+    # overriding abstract method
+    def area(self) -> float:
+        return self.short * self.long
+        
 
 # Exercise 2: Implementing Static Methods
 
 # Create a class MathOperations with a static method add that takes two numbers and returns their sum,
 #    and another static method multiply that takes two numbers and returns their product.
 
+class MathOperations:
+
+    @staticmethod
+    def sum(num1: float, num2: float) -> float:
+        return num1 + num2
+    
+    @staticmethod
+    def multiply(num1: float, num2: float) -> float:
+        return num1 * num2
 
 
 # Exercise 3: Library Management System 
@@ -32,6 +76,20 @@ from abc import ABC, abstractmethod
 
 # title = La Divina Commedia, author = D. Alighieri, isbn = 999000666
 
+class Book:
+    def __init__(self, title: str, author: str, isbn: str) -> None:
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+
+    def __str__(self) -> str:
+        return f"Title: {self.title}\nAuthor: {self.author}\nIsbn: {self.isbn}"
+    
+    @classmethod
+    def from_string(cls, book_str: str):
+        string_list: list= book_str.split(', ')
+        return Book(string_list[0], string_list[1], string_list[2])
+
 # Create a Member class with the following attributes: name, member_id, borrowed_books
 # The member class must contain the following methods:
 
@@ -43,6 +101,26 @@ from abc import ABC, abstractmethod
 
 #     @classmethod from_string(cls, member_str) to create a Member instance from a string in the format
 #            "name, member_id".
+
+class Member:
+    def __init__(self, name: str, member_id: str) -> None:
+        self.name = name
+        self.member_id = member_id
+        self.borrowed_books: list[Book] = []
+    
+    def borrow_book(self, book: Book) -> None:
+        self.borrowed_books.append(book)
+
+    def return_book(self, book: Book) -> None:
+        self.borrowed_books.remove(book)
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}\nMember ID: {self.member_id}\nBorrowed books: {self.borrowed_books}"
+    
+    @classmethod
+    def from_string(cls, member_str: str):
+        string_list: list= member_str.split(', ')
+        return Member(string_list[0], string_list[1])
 
 # Create a Library class with the following attributes: books, members, total_books (class attribute to keep
 #    track of the total number of books)
@@ -61,9 +139,34 @@ from abc import ABC, abstractmethod
 
 #     @classmethod library_statistics(cls) to print the total number of books.
 
+class Library:
+    total_books: int= 0
+    def __init__(self) -> None:
+        self.books: list[Book]= []
+        self.members: list[Member]= []
+
+    def add_book(self, book: Book) -> None:
+        self.books.append(book)
+        Library.total_books += 1
+
+    def remove_book(self, book: Book) -> None:
+        self.books.remove(book)
+        Library.total_books -= 1
+
+    def register_member(self, member: Member) -> None:
+        self.members.append(member)
+
+    def lend_book(self, book: Book, member: Member) -> None:
+        if book in self.books:
+            if member in self.members:
+                member.borrow_book(book)
+
 # Create a script and play a bit with the classes:
 # Create instances of books and members using class methods. Register members and add books to the library.
 #    Lend books to members and display the state of the library before and after lending.
+
+
+
 
 # Exercise 4: University Management System
 
